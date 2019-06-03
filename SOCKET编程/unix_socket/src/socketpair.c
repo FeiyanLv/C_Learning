997f2ff0e3a307ed19f39d36bbab5728a8dbc416
vousTec <vous1158@gmail.com>
2019-06-03 15:17:20 +0800

    进程间发送文件描述符

--- SOCKET编程/unix_socket/src/socketpair.c
+++ SOCKET编程/unix_socket/src/socketpair.c
@@ -1,13 +1,7 @@
-//
-// Created by liuxing1 on 6/3/19.
-//
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include "apue.h"
 
 #define ERR_EXIT(m)\
         do\
@@ -19,6 +13,7 @@
 
 int main(void) {
     int sockfds[2];
+
     if (socketpair(PF_UNIX, SOCK_STREAM, 0, sockfds) < 0)
         ERR_EXIT("socketpair");
     pid_t pid;
@@ -26,18 +21,24 @@ int main(void) {
     if (pid == -1)
         ERR_EXIT("fork");
     else if (0 == pid) {
+        int val;
         close(sockfds[0]);
-        int fd;
-        fd = open("../tmp/test.txt", O_RDONLY);
-        if (fd == -1)
-            ERR_EXIT("open");
-        send_fd(sockfds[1], fd);
+        while (1) {
+            read(sockfds[1], &val, sizeof(val));
+            ++val;
+            write(sockfds[1], &val, sizeof(val));
+        }
     } else {
+        int val = 0;
         close(sockfds[1]);
-        int fd = recv_fd(sockfds[0], NULL);
-        char buf[1024] = {0};
-        read(fd, buf, sizeof(buf));
-        printf("%s", buf);
+        while (1) {
+            ++val;
+            printf("sending data: %d\n", val);
+            write(sockfds[0], &val, sizeof(val));
+            read(sockfds[0], &val, sizeof(val));
+            printf("data received: %d\n", val);
+            sleep(1);
+        }
     }
     return 0;
 }
