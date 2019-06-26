4c7278dc688953ad2fa8273fb29c4494028482ad
vousTec <vous1158@gmail.com>
2019-06-26 15:25:07 +0800

    ipc echo bug fix

--- SOCKET编程/ipc_echo/src/echo_srv.c
+++ SOCKET编程/ipc_echo/src/echo_srv.c
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/ipc.h>
 #include <sys/msg.h>
 
 #define ERR_EXIT(m)\
@@ -25,36 +24,26 @@ typedef struct msgbuf {
 void echo_srv(int msgid);
 
 int main(void) {
-    int msgid;
-    msgid = msgget(1234, IPC_CREAT | 0666);
+    int msgid = msgget(1234, IPC_CREAT | 0666);
     if (msgid == -1)
         ERR_EXIT("msgget");
 
-    printf("%d", msgid);
     echo_srv(msgid);
 
     return 0;
 }
 
 void echo_srv(int msgid) {
-    printf("%d", msgid);
-
     msgbuf buf;
-    memset(&buf, 0, sizeof(buf));
     int n;
     while (1) {
+        memset(&buf, 0, sizeof(buf));
         if ((n = msgrcv(msgid, &buf, MSGMAX, 1, 0)) < 0)
             ERR_EXIT("msgrcv");
 
-        printf("%s", buf.mtext);
-
-        int pid;
-        pid = *((int *) buf.mtext);
+        int pid = *((int *) buf.mtext);
         fputs(buf.mtext + 4, stdout);
         buf.mtype = pid;
-
-        printf("%s", buf.mtext);
-
         msgsnd(msgid, &buf, n, 0);
     }
 }
