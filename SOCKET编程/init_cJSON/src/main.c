c850f4298e29a596d3b8003be6696651c237aa49
vousTec <vous1158@gmail.com>
2019-07-03 14:17:05 +0800

    cjson

--- SOCKET编程/init_cJSON/src/main.c
+++ SOCKET编程/init_cJSON/src/main.c
@@ -7,21 +7,57 @@
 #include <string.h>
 #include <cJSON.c>
 
+char *create_js(void);
+
+int parse_js(char *out);
+
+
 int main() {
+    create_js();
+    parse_js(create_js());
+    return 0;
+}
 
-    cJSON *usr;
+char *create_js(void) {
+    cJSON *usr1, *usr2, *array, *root;
 
-    usr = cJSON_CreateObject();                         //创建根数据对象
-    cJSON_AddStringToObject(usr, "name", "liuxingj");   //加入键值，加字符串
-    cJSON_AddStringToObject(usr, "passwd", "12345678");
-    cJSON_AddNumberToObject(usr, "num", 3);             //加整数
+    root = cJSON_CreateObject();
+    array = cJSON_CreateArray();
+    usr1 = cJSON_CreateObject();
+    usr2 = cJSON_CreateObject();
 
-    char *out = cJSON_Print(usr);                       //将json形式打印成正常字符串形式
+    cJSON_AddItemToObject(root, "body", array);
+    cJSON_AddItemToArray(array, usr1);
+    cJSON_AddItemToArray(array, usr2);
+
+    cJSON_AddStringToObject(usr1, "name", "liuxingj");      //加入键值，加字符串
+    cJSON_AddStringToObject(usr1, "passwd", "12345678");
+    cJSON_AddNumberToObject(usr1, "num", 3);             //加整数
+
+    cJSON_AddStringToObject(usr2, "name", "lihaoy");   //加入键值，加字符串
+    cJSON_AddStringToObject(usr2, "passwd", "12345678");
+    cJSON_AddNumberToObject(usr2, "num", 3);             //加整数
+
+    char *out = cJSON_PrintUnformatted(root);                       //将json形式打印成正常字符串形式
     printf("%s\n", out);
 
     // 释放内存
-    cJSON_Delete(usr);
-    free(out);
-    return 0;
+    cJSON_Delete(root);
+    return out;
+}
 
+int parse_js(char *out) {
+    cJSON *jsParser;
+    jsParser = cJSON_Parse(out);
+    cJSON *usr1, *usr2, *js_array, *js_name, *js_password, *js_num;
+
+    js_array = cJSON_GetObjectItem(jsParser, "body");
+
+    usr1 = cJSON_GetArrayItem(js_array, 1);
+
+    js_name = cJSON_GetObjectItem(usr1, "name");
+    js_password = cJSON_GetObjectItem(usr1, "passwd");
+    js_num = cJSON_GetObjectItem(usr1, "num");
+
+    printf("%s:%s:%d", js_name->valuestring, js_password->valuestring, js_num->valueint);
 }
